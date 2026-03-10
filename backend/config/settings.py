03ee074ee5325829bb983204c0b9799ef4f08ea9
Aung Kyaw Zin <aungkyawzin2432003@gmail.com>
2026-03-10 00:48:52 +0000

    Fix railway.json

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -86,3 +87,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5173",
 ]
+# Production static files
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Production security
+CSRF_TRUSTED_ORIGINS = []  # We'll fill this after deployment
